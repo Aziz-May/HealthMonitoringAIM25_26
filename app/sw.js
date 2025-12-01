1a25efc3d4ca9fe80f326847f0b6b25351e0b48a
Amin Chamtouri <mohamedamine.chamtouri@supcom.tn>
2025-12-01 22:30:12 +0100

    Fix PWA installation and UI improvements

--- app/sw.js
+++ app/sw.js
@@ -1,25 +1,23 @@
-const cacheName = "smarthealth-v1";
+const cacheName = "smarthealth-v2";
 const assets = [
-  "/",
-  "/scripts/events.js",
-  "/scripts/iam.js",
-  "/scripts/router.js",
-  "/scripts/main.js", 
-  "/scripts/home.js",
-  "/scripts/mvp.js",
-  "/scripts/routes.js",
-  "/scripts/util.js",
-  "/scripts/websocket.js",
-  "/pages/dashboard.html",
-  "/styles/main.css"
-  // Icons temporarily disabled - create these later:
-  // "/images/icons/android-chrome-192x192.png",
-  // "/images/icons/android-chrome-512x512.png",
-  // "/images/icons/apple-touch-icon.png",
-  // "/images/icons/favicon-16x16.png",
-  // "/images/icons/favicon-32x32.png",
-  // "/images/logo.png",
-  // "/images/profile.png"
+  "./",
+  "./index.html",
+  "./scripts/events.js",
+  "./scripts/iam.js",
+  "./scripts/router.js",
+  "./scripts/main.js", 
+  "./scripts/home.js",
+  "./scripts/mvp.js",
+  "./scripts/routes.js",
+  "./scripts/util.js",
+  "./scripts/websocket.js",
+  "./pages/dashboard.html",
+  "./styles/main.css",
+  "./images/icons/android-chrome-192x192.png",
+  "./images/icons/android-chrome-512x512.png",
+  "./images/icons/apple-touch-icon.png",
+  "./images/icons/favicon-16x16.png",
+  "./images/icons/favicon-32x32.png"
 ];
 
 // Clear old caches
